ac250a6d4ab596515dfcfe026e79387d97269d07
afsar01 <48503678+afsar01@users.noreply.github.com>
2019-06-07 11:48:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,5 @@ from flask import Flask
 from redis import Redis
 
 app = Flask(__name__)
-redis = Redis(host='redis', port=6379)
+print ('Welcome to Java Home Python App')
 
-@app.route('/')
-def hello():
-     count = redis.incr('hits')
-    return 'Welcome to Java Home Python App - Number of hits = {}'.format(count)
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
